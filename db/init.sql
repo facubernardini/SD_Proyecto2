8650db5e199ebce0b8f1ad173172a34baaae76c1
Facundo Bernardini <facubernardini@hotmail.com>
2025-07-03 22:42:06 -0300

    Agregue dos tablas de relaciones
    ahora se respeta la estructura de la base de datos inicial

--- db/init.sql
+++ db/init.sql
@@ -1,37 +1,49 @@
 USE consorcio;
 
 CREATE TABLE clientes (
-    id_cliente INT PRIMARY KEY,
-    password_cliente VARCHAR(255) NOT NULL,
-    nombre VARCHAR(100) NOT NULL,
-    email VARCHAR(150) UNIQUE NOT NULL
+  id_cliente INT PRIMARY KEY,
+  password_cliente VARCHAR(255) NOT NULL,
+  nombre VARCHAR(100) NOT NULL,
+  email VARCHAR(150) UNIQUE NOT NULL
 );
 
 CREATE TABLE categorias (
-    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) UNIQUE NOT NULL
-);
-
-CREATE TABLE cliente_categoria (
-    id_cliente INT,
-    id_categoria INT,
-    PRIMARY KEY (id_cliente, id_categoria),
-
-    FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente) ON DELETE CASCADE,
-    FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria) ON DELETE CASCADE
+  id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(100) UNIQUE NOT NULL
 );
 
 CREATE TABLE noticias (
-    id_noticia INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(255) NOT NULL,
-    contenido TEXT NOT NULL,
-    time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  id_noticia INT AUTO_INCREMENT PRIMARY KEY,
+  titulo VARCHAR(255) NOT NULL,
+  contenido TEXT NOT NULL,
+  time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-    id_cliente INT,
-    FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente) ON DELETE SET NULL,
+CREATE TABLE cliente_categoria (
+  id_cliente INT,
+  id_categoria INT,
+  PRIMARY KEY (id_cliente, id_categoria),
 
-    id_categoria INT,
-    FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria) ON DELETE SET NULL
+  FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente) ON DELETE CASCADE,
+  FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria) ON DELETE CASCADE
+);
+
+CREATE TABLE cliente_noticia (
+  id_cliente INT,
+  id_noticia INT,
+  PRIMARY KEY (id_cliente, id_noticia),
+
+  FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente) ON DELETE CASCADE,
+  FOREIGN KEY (id_noticia) REFERENCES noticias(id_noticia) ON DELETE CASCADE
+);
+
+CREATE TABLE noticia_categoria (
+  id_noticia INT,
+  id_categoria INT,
+  PRIMARY KEY (id_noticia, id_categoria),
+
+  FOREIGN KEY (id_noticia) REFERENCES noticias(id_noticia) ON DELETE CASCADE,
+  FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria) ON DELETE CASCADE
 );
 
 #------------------ VISTAS ------------------#
@@ -53,47 +65,57 @@ SELECT
   cli.nombre AS cliente,
   cat.nombre AS categoria
 FROM noticias n
-LEFT JOIN clientes cli ON n.id_cliente = cli.id_cliente
-LEFT JOIN categorias cat ON n.id_categoria = cat.id_categoria
+LEFT JOIN cliente_noticia cn ON n.id_noticia = cn.id_noticia
+LEFT JOIN clientes cli ON cn.id_cliente = cli.id_cliente
+LEFT JOIN noticia_categoria nc ON n.id_noticia = nc.id_noticia
+LEFT JOIN categorias cat ON nc.id_categoria = cat.id_categoria
 WHERE n.time_stamp >= NOW() - INTERVAL 1 DAY;
 
 #-- Ver todas las noticias que creó el cliente
 CREATE VIEW vista_noticias_creadas_cliente AS
 SELECT 
+  cli.id_cliente,
+  cli.nombre AS autor,
   n.id_noticia,
   n.titulo,
   n.contenido,
-  n.time_stamp,
-  cli.id_cliente,
-  cli.nombre AS autor,
-  cat.nombre AS nombre_categoria
-FROM noticias n
-LEFT JOIN clientes cli ON n.id_cliente = cli.id_cliente
-LEFT JOIN categorias cat ON n.id_categoria = cat.id_categoria;
+  n.time_stamp
+FROM cliente_noticia cn
+JOIN clientes cli ON cn.id_cliente = cli.id_cliente
+JOIN noticias n ON cn.id_noticia = n.id_noticia;
 
 #-- Ver a que categorias está suscripto el cliente
 CREATE VIEW vista_suscripciones_cliente AS
 SELECT 
-  cc.id_cliente,
+  cli.id_cliente,
+  cli.nombre AS cliente,
   cat.id_categoria,
   cat.nombre AS categoria
 FROM cliente_categoria cc
+JOIN clientes cli ON cc.id_cliente = cli.id_cliente
 JOIN categorias cat ON cc.id_categoria = cat.id_categoria;
 
 #-- Ver todas las noticias de las categorias a las que está suscripto el cliente
 CREATE VIEW vista_noticias_para_cliente AS
 SELECT 
   cc.id_cliente,
+  cli.nombre AS cliente,
   n.id_noticia,
   n.titulo,
   n.contenido,
   n.time_stamp,
   cat.nombre AS categoria,
-  cli.nombre AS autor
+  autor.id_cliente AS id_autor,
+  autor.nombre AS nombre_autor
 FROM cliente_categoria cc
-JOIN noticias n ON cc.id_categoria = n.id_categoria
-LEFT JOIN categorias cat ON n.id_categoria = cat.id_categoria
-LEFT JOIN clientes cli ON n.id_cliente = cli.id_cliente;
+JOIN categorias cat ON cc.id_categoria = cat.id_categoria
+JOIN noticia_categoria nc ON cat.id_categoria = nc.id_categoria
+JOIN noticias n ON nc.id_noticia = n.id_noticia
+
+LEFT JOIN cliente_noticia cn ON n.id_noticia = cn.id_noticia
+LEFT JOIN clientes autor ON cn.id_cliente = autor.id_cliente
+
+JOIN clientes cli ON cc.id_cliente = cli.id_cliente;
 
 #-- Ver las nuevas noticias (última hora)
 CREATE VIEW vista_nuevas_noticias_para_enviar AS
@@ -101,16 +123,26 @@ SELECT
   cc.id_cliente,
   cli.nombre AS nombre_cliente,
   cli.email,
+
   n.id_noticia,
   n.titulo,
   n.contenido,
   n.time_stamp,
-  cat.nombre AS categoria
+  
+  cat.id_categoria,
+  cat.nombre AS nombre_categoria,
+
+  autor.id_cliente AS id_autor,
+  autor.nombre AS nombre_autor
 FROM noticias n
-JOIN categorias cat ON n.id_categoria = cat.id_categoria
-JOIN cliente_categoria cc ON n.id_categoria = cc.id_categoria
+JOIN noticia_categoria nc ON n.id_noticia = nc.id_noticia
+JOIN categorias cat ON nc.id_categoria = cat.id_categoria
+
+JOIN cliente_categoria cc ON cat.id_categoria = cc.id_categoria
 JOIN clientes cli ON cc.id_cliente = cli.id_cliente
-WHERE n.time_stamp >= NOW() - INTERVAL 1 HOUR;
+
+LEFT JOIN cliente_noticia cn ON n.id_noticia = cn.id_noticia
+LEFT JOIN clientes autor ON cn.id_cliente = autor.id_cliente;
 
 
 #------------------ DATOS DE PRUEBA ------------------#
@@ -119,15 +151,23 @@ INSERT INTO clientes(id_cliente, password_cliente, nombre, email) VALUES (414600
 INSERT INTO clientes(id_cliente, password_cliente, nombre, email) VALUES (41460005, MD5('prueba'), 'Pepe', 'prueba2@hola.com');
 INSERT INTO clientes(id_cliente, password_cliente, nombre, email) VALUES (41460006, MD5('prueba'), 'Pablo', 'prueba3@hola.com');
 
-INSERT INTO categorias(id_categoria, nombre) VALUES (1, 'Policial');
-INSERT INTO categorias(id_categoria, nombre) VALUES (2, 'Deportiva');
-INSERT INTO categorias(id_categoria, nombre) VALUES (3, 'Finanzas');
+INSERT INTO categorias(nombre) VALUES ('Policial');
+INSERT INTO categorias(nombre) VALUES ('Deportiva');
+INSERT INTO categorias(nombre) VALUES ('Finanzas');
+
+INSERT INTO noticias(titulo, contenido) VALUES ('Choque sobre ruta 3', 'Por la madrugada del lunes chocaron dos vehiculos. No se registraron heridos.');
+INSERT INTO noticias(titulo, contenido) VALUES ('Cientificos reviven al Diego', 'Segun fuentes de tiktok aseguran haber revivido al Diegote pero ahora patea con la derecha');
+INSERT INTO noticias(titulo, contenido) VALUES ('Vuela el dolar', 'El dolar toca los $23049 por unidad');
 
 INSERT INTO cliente_categoria(id_cliente, id_categoria) VALUES (41460004, 1);
 INSERT INTO cliente_categoria(id_cliente, id_categoria) VALUES (41460004, 2);
 INSERT INTO cliente_categoria(id_cliente, id_categoria) VALUES (41460005, 2);
 INSERT INTO cliente_categoria(id_cliente, id_categoria) VALUES (41460006, 3);
 
-INSERT INTO noticias(titulo, contenido, id_cliente, id_categoria) VALUES ('Cientificos reviven al Diego', 'Segun fuentes de tiktok aseguran haber revivido al Diegote pero ahora patea con la derecha', 41460004, 2);
-INSERT INTO noticias(titulo, contenido, id_cliente, id_categoria) VALUES ('Choque sobre ruta 3', 'Por la madrugada del lunes chocaron dos vehiculos. No se registraron heridos.', 41460004, 1);
-INSERT INTO noticias(titulo, contenido, id_cliente, id_categoria) VALUES ('Vuela el dolar', 'El dolar toca los $23049 por unidad', 41460005, 3);
+INSERT INTO cliente_noticia(id_cliente, id_noticia) VALUES (41460004, 1);
+INSERT INTO cliente_noticia(id_cliente, id_noticia) VALUES (41460004, 2);
+INSERT INTO cliente_noticia(id_cliente, id_noticia) VALUES (41460005, 3);
+
+INSERT INTO noticia_categoria(id_noticia, id_categoria) VALUES (1, 1);
+INSERT INTO noticia_categoria(id_noticia, id_categoria) VALUES (2, 2);
+INSERT INTO noticia_categoria(id_noticia, id_categoria) VALUES (3, 3);
