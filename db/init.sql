816e08a4554a2e32d622cc75155963053e76a97f
Facundo Bernardini <facubernardini@hotmail.com>
2025-07-03 21:38:41 -0300

    Arreglos en la base de datos

--- db/init.sql
+++ db/init.sql
@@ -41,60 +41,93 @@ CREATE VIEW vista_categorias_disponibles AS
 SELECT 
   id_categoria,
   nombre AS nombre_categoria
-FROM 
-  categorias;
+FROM categorias;
 
 #-- Ver todas las noticias de las últimas 24 hs 
 CREATE VIEW vista_noticias_ultimas_24hs AS
 SELECT 
-    n.id_noticia,
-    n.titulo,
-    n.contenido,
-    n.time_stamp,
-    cli.nombre AS cliente,
-    cat.nombre AS categoria
+  n.id_noticia,
+  n.titulo,
+  n.contenido,
+  n.time_stamp,
+  cli.nombre AS cliente,
+  cat.nombre AS categoria
 FROM noticias n
 LEFT JOIN clientes cli ON n.id_cliente = cli.id_cliente
 LEFT JOIN categorias cat ON n.id_categoria = cat.id_categoria
 WHERE n.time_stamp >= NOW() - INTERVAL 1 DAY;
 
 #-- Ver todas las noticias que creó el cliente
-CREATE VIEW vista_noticias_del_cliente AS
+CREATE VIEW vista_noticias_creadas_cliente AS
 SELECT 
   n.id_noticia,
   n.titulo,
   n.contenido,
   n.time_stamp,
-  c.id_cliente,
-  c.nombre AS autor,
+  cli.id_cliente,
+  cli.nombre AS autor,
   cat.nombre AS nombre_categoria
-FROM 
-  noticias n
-JOIN 
-  clientes c ON n.id_cliente = c.id_cliente
-JOIN 
-  categorias cat ON n.id_categoria = cat.id_categoria;
+FROM noticias n
+LEFT JOIN clientes cli ON n.id_cliente = cli.id_cliente
+LEFT JOIN categorias cat ON n.id_categoria = cat.id_categoria;
 
 #-- Ver a que categorias está suscripto el cliente
 CREATE VIEW vista_suscripciones_cliente AS
 SELECT 
-    cc.id_cliente,
-    c.id_categoria,
-    c.nombre AS categoria
+  cc.id_cliente,
+  cat.id_categoria,
+  cat.nombre AS categoria
 FROM cliente_categoria cc
-JOIN categorias c ON cc.id_categoria = c.id_categoria;
+JOIN categorias cat ON cc.id_categoria = cat.id_categoria;
 
-#------------------ DATOS DE PRUEBA (borrar) ------------------#
+#-- Ver todas las noticias de las categorias a las que está suscripto el cliente
+CREATE VIEW vista_noticias_para_cliente AS
+SELECT 
+  cc.id_cliente,
+  n.id_noticia,
+  n.titulo,
+  n.contenido,
+  n.time_stamp,
+  cat.nombre AS categoria,
+  cli.nombre AS autor
+FROM cliente_categoria cc
+JOIN noticias n ON cc.id_categoria = n.id_categoria
+LEFT JOIN categorias cat ON n.id_categoria = cat.id_categoria
+LEFT JOIN clientes cli ON n.id_cliente = cli.id_cliente;
+
+#-- Ver las nuevas noticias (última hora)
+CREATE VIEW vista_nuevas_noticias_para_enviar AS
+SELECT 
+  cc.id_cliente,
+  cli.nombre AS nombre_cliente,
+  cli.email,
+  n.id_noticia,
+  n.titulo,
+  n.contenido,
+  n.time_stamp,
+  cat.nombre AS categoria
+FROM noticias n
+JOIN categorias cat ON n.id_categoria = cat.id_categoria
+JOIN cliente_categoria cc ON n.id_categoria = cc.id_categoria
+JOIN clientes cli ON cc.id_cliente = cli.id_cliente
+WHERE n.time_stamp >= NOW() - INTERVAL 1 HOUR;
+
+
+#------------------ DATOS DE PRUEBA ------------------#
 
 INSERT INTO clientes(id_cliente, password_cliente, nombre, email) VALUES (41460004, MD5('prueba'), 'Facundo', 'prueba@hola.com');
 INSERT INTO clientes(id_cliente, password_cliente, nombre, email) VALUES (41460005, MD5('prueba'), 'Pepe', 'prueba2@hola.com');
+INSERT INTO clientes(id_cliente, password_cliente, nombre, email) VALUES (41460006, MD5('prueba'), 'Pablo', 'prueba3@hola.com');
 
 INSERT INTO categorias(id_categoria, nombre) VALUES (1, 'Policial');
 INSERT INTO categorias(id_categoria, nombre) VALUES (2, 'Deportiva');
+INSERT INTO categorias(id_categoria, nombre) VALUES (3, 'Finanzas');
 
 INSERT INTO cliente_categoria(id_cliente, id_categoria) VALUES (41460004, 1);
 INSERT INTO cliente_categoria(id_cliente, id_categoria) VALUES (41460004, 2);
 INSERT INTO cliente_categoria(id_cliente, id_categoria) VALUES (41460005, 2);
+INSERT INTO cliente_categoria(id_cliente, id_categoria) VALUES (41460006, 3);
 
 INSERT INTO noticias(titulo, contenido, id_cliente, id_categoria) VALUES ('Cientificos reviven al Diego', 'Segun fuentes de tiktok aseguran haber revivido al Diegote pero ahora patea con la derecha', 41460004, 2);
 INSERT INTO noticias(titulo, contenido, id_cliente, id_categoria) VALUES ('Choque sobre ruta 3', 'Por la madrugada del lunes chocaron dos vehiculos. No se registraron heridos.', 41460004, 1);
+INSERT INTO noticias(titulo, contenido, id_cliente, id_categoria) VALUES ('Vuela el dolar', 'El dolar toca los $23049 por unidad', 41460005, 3);
