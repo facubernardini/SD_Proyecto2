d2e66c65accc3b3c41d2818fabc0df1703f4f1b3
MiguelBustamante <111795113+miguelbustamante2020@users.noreply.github.com>
2025-07-05 05:53:38 -0300

    Update miembro.py

--- agente/miembro.py
+++ agente/miembro.py
@@ -8,6 +8,7 @@ import suscripciones_noticias_pb2_grpc as pb2_grpc
 import time
 import os
 from cryptography.fernet import Fernet
+import hashlib
 
 class ServicioSuscripciones(pb2_grpc.SuscripcionesNoticiasServicer):
     def __init__(self, db):
@@ -19,31 +20,36 @@ class ServicioSuscripciones(pb2_grpc.SuscripcionesNoticiasServicer):
         try:
             # descifrar password
             password_plano = self.fernet.decrypt(request.password.encode()).decode()
-            # verificar cliente con contraseña
-            cursor.execute("SELECT id_cliente FROM clientes WHERE id_cliente=%s AND password=%s", (request.cliente_id, password_plano))
+            # calcular el md5
+            password_md5 = hashlib.md5(password_plano.encode()).hexdigest()
+            # verificar cliente
+            cursor.execute(
+                "SELECT id_cliente FROM clientes WHERE id_cliente=%s AND password_cliente=%s",
+                (request.cliente_id, password_md5)
+            )
             cliente = cursor.fetchone()
             if not cliente:
-                return pb2.Respuesta(mensaje=f"El cliente  {request.cliente_id} no existe", exito=False)
+                return pb2.Respuesta(mensaje=f"El cliente {request.cliente_id} o contraseña incorrecta", exito=False)
 
             cursor.execute("SELECT id_categoria FROM categorias WHERE nombre=%s", (request.area,))
             area = cursor.fetchone()
             if not area:
-                return pb2.Respuesta(mensaje=f"El Ã¡rea {request.area} no existe", exito=False)
+                return pb2.Respuesta(mensaje=f"El área {request.area} no existe", exito=False)
 
-            cursor.execute("""
-                SELECT * FROM cliente_categoria
-                WHERE id_cliente=%s AND id_categoria=%s
-            """, (request.cliente_id, area[0]))
+            cursor.execute(
+                "SELECT * FROM cliente_categoria WHERE id_cliente=%s AND id_categoria=%s",
+                (request.cliente_id, area[0])
+            )
             existe = cursor.fetchone()
             if existe:
                 return pb2.Respuesta(mensaje=f"El cliente {request.cliente_id} ya estaba suscripto a {request.area}", exito=False)
 
-            cursor.execute("""
-                INSERT INTO cliente_categoria (id_cliente, id_categoria)
-                VALUES (%s, %s)
-            """, (request.cliente_id, area[0]))
+            cursor.execute(
+                "INSERT INTO cliente_categoria (id_cliente, id_categoria) VALUES (%s, %s)",
+                (request.cliente_id, area[0])
+            )
             self.db.commit()
-            return pb2.Respuesta(mensaje=f"{request.cliente_id} se suscribiÃ³ a {request.area}", exito=True)
+            return pb2.Respuesta(mensaje=f"{request.cliente_id} se suscribió a {request.area}", exito=True)
 
         except Exception as e:
             print(f"Error en SubscribirCliente: {e}")
@@ -55,64 +61,57 @@ class ServicioSuscripciones(pb2_grpc.SuscripcionesNoticiasServicer):
             # desencriptar contraseña enviada
             password_plano = self.fernet.decrypt(request.password.encode()).decode()
 
-            # verificar cliente con contraseña
-            cursor.execute("SELECT id_cliente FROM clientes WHERE id_cliente=%s AND password=%s", (request.cliente_id, password_plano))
+            password_md5 = hashlib.md5(password_plano.encode()).hexdigest()
+
+            # verificar cliente
+            cursor.execute(
+                "SELECT id_cliente FROM clientes WHERE id_cliente=%s AND password_cliente=%s",
+                (request.cliente_id, password_md5)
+            )
             cliente = cursor.fetchone()
             if not cliente:
-                return pb2.Respuesta(mensaje="El cliente o contrasena no existe", exito=False)
+                return pb2.Respuesta(mensaje="El cliente o contraseña incorrecta", exito=False)
 
             cursor.execute("SELECT id_categoria FROM categorias WHERE nombre=%s", (request.area,))
             area = cursor.fetchone()
             if not area:
-                return pb2.Respuesta(mensaje=f"El area {request.area} no existe", exito=False)
+                return pb2.Respuesta(mensaje=f"El área {request.area} no existe", exito=False)
 
-            cursor.execute("""
-                SELECT * FROM cliente_categoria
-                WHERE id_cliente=%s AND id_categoria=%s
-            """, (request.cliente_id, area[0]))
+            cursor.execute(
+                "SELECT * FROM cliente_categoria WHERE id_cliente=%s AND id_categoria=%s",
+                (request.cliente_id, area[0])
+            )
             existe = cursor.fetchone()
             if not existe:
                 return pb2.Respuesta(mensaje=f"El cliente {request.cliente_id} no estaba suscripto a {request.area}", exito=False)
 
-            cursor.execute("""
-                DELETE FROM cliente_categoria
-                WHERE id_cliente=%s AND id_categoria=%s
-            """, (request.cliente_id, area[0]))
+            cursor.execute(
+                "DELETE FROM cliente_categoria WHERE id_cliente=%s AND id_categoria=%s",
+                (request.cliente_id, area[0])
+            )
             self.db.commit()
-            return pb2.Respuesta(mensaje=f"{request.cliente_id} se desuscribio de {request.area}", exito=True)
+            return pb2.Respuesta(mensaje=f"{request.cliente_id} se desuscribió de {request.area}", exito=True)
 
         except Exception as e:
             print(f"Error en BorrarSuscripcion: {e}")
-            return pb2.Respuesta(mensaje="Error al borrar suscripcion", exito=False)
+            return pb2.Respuesta(mensaje="Error al borrar suscripción", exito=False)
             
-    def ObtenerClientesPorArea(self, request, context):
-        cursor = self.db.cursor()
-        try:
-            cursor.execute("""
-                SELECT c.id_cliente
-                FROM clientes c
-                JOIN cliente_categoria cc ON c.id_cliente = cc.id_cliente
-                JOIN categorias cat ON cc.id_categoria = cat.id_categoria
-                WHERE cat.nombre=%s
-            """, (request.nombre,))
-            ids = [str(row[0]) for row in cursor.fetchall()]
-            return pb2.ListaClientes(clientes=ids)
-        except Exception as e:
-            print(f"Error en ObtenerClientesPorArea: {e}")
-            return pb2.ListaClientes(clientes=[])
-
     def ObtenerNoticiasDeArea(self, request, context):
         cursor = self.db.cursor()
         try:
             # desencriptar contraseña enviada
             password_plano = self.fernet.decrypt(request.password.encode()).decode()
 
-            # verificar cliente con contraseña
-            cursor.execute("SELECT id_cliente FROM clientes WHERE id_cliente=%s AND password=%s", (request.cliente_id, password_plano))
+            password_md5 = hashlib.md5(password_plano.encode()).hexdigest()
+
+            cursor.execute(
+                "SELECT id_cliente FROM clientes WHERE id_cliente=%s AND password_cliente=%s",
+                (request.cliente_id, password_md5)
+            )
             cliente = cursor.fetchone()
             if not cliente:
                 return pb2.ListaNoticias(noticias=[])
-            
+
             cursor.execute("SELECT id_categoria FROM categorias WHERE nombre=%s", (request.area,))
             area = cursor.fetchone()
             if not area:
@@ -121,7 +120,8 @@ class ServicioSuscripciones(pb2_grpc.SuscripcionesNoticiasServicer):
             cursor.execute("""
                 SELECT titulo, contenido, time_stamp
                 FROM noticias
-                WHERE id_categoria=%s
+                JOIN noticia_categoria nc ON noticias.id_noticia = nc.id_noticia
+                WHERE nc.id_categoria=%s
                 ORDER BY time_stamp DESC
                 LIMIT 3
             """, (area[0],))
@@ -134,11 +134,9 @@ class ServicioSuscripciones(pb2_grpc.SuscripcionesNoticiasServicer):
             ]
             return pb2.ListaNoticias(noticias=noticias)
 
-
         except Exception as e:
             print(f"Error en ObtenerNoticiasDeArea: {e}")
             return pb2.ListaNoticias(noticias=[])
-
 
 def main():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
