b417eb716a0a8ce1b1f155e371c8289fd8a7570f
MiguelBustamante <111795113+miguelbustamante2020@users.noreply.github.com>
2025-07-11 16:33:23 -0300

    Update cliente_simulador.py

--- noticias_consorcio/cliente/cliente_simulador.py
+++ noticias_consorcio/cliente/cliente_simulador.py
@@ -1,4 +1,4 @@
-﻿# -*- coding: utf-8 -*-
+﻿﻿# -*- coding: utf-8 -*-
 import grpc
 from cryptography.fernet import Fernet
 import sys
@@ -10,35 +10,47 @@ CLAVE_FERNET = b'J8vKqAnGsyiQUKmM2hRsaM4TQEL8gtjCKxgMrzG2Fnw='
 cifrador = Fernet(CLAVE_FERNET)
 
 def run():
-    channel = grpc.insecure_channel("localhost:50051")
+    channel = grpc.insecure_channel("localhost:50055")
     stub = pb2_grpc.SuscripcionesNoticiasStub(channel)
 
-    password_cifrado = cifrador.encrypt(b"1234").decode()
+    # while True:
+    #     entrada = input("Ingresá su número de documento: ").strip()
+    #     if entrada.isdigit():
+    #         id_cliente = int(entrada)  #Lo transoformo a entero
+    #         break
+    #     else:
+    #         print("Ingresá un número de documento válido.")
+    password = "prueba"
+    password_cifrado = cifrador.encrypt(password.encode())
 
-    resp1 = stub.SubscribirCliente(pb2.ClienteArea(
-        cliente_id=1,
-        area="Policial",
-        password=password_cifrado
-    ))
-    print("SubscribirCliente:", resp1.mensaje)
+    # area = input("Ingrese el nuevo area\n")
+    # resp1 = stub.SubscribirCliente(pb2.ClienteArea(
+    #     cliente_id=41460004,
+    #     area=area,
+    #     password=password_cifrado
+    # ))
+    # print("SubscribirCliente:", resp1.mensaje)
 
+    area = input("Ingrese el area a desuscribir\n")
     resp2 = stub.BorrarSuscripcion(pb2.ClienteArea(
-        cliente_id=1,
-        area="Policial",
+        cliente_id=41460004,
+        area=area,
         password=password_cifrado
     ))
     print("BorrarSuscripcion:", resp2.mensaje)
 
-    lista = stub.ObtenerClientesPorArea(pb2.Area(nombre="Deportiva"))
-    print("Clientes en Deportiva:", lista.clientes)
+    # lista = stub.ObtenerClientesPorArea(pb2.area(nombre="Deportiva"))
+    # print("Clientes en Deportiva:", lista.clientes)
 
-    noticias = stub.ObtenerNoticiasDeArea(pb2.ClienteArea(
-        cliente_id=1,
-        area="Deportiva",
-        password=password_cifrado
-    ))
-    for n in noticias.noticias:
-        print(f"[{n.fecha}] {n.titulo}: {n.contenido}")
+    # area = input("Ingrese el area a desuscribir\n")
+    # noticias = stub.ObtenerNoticiasDeArea(pb2.ClienteArea(
+    #     cliente_id=41460004,
+    #     area=area,
+    #     password=password_cifrado
+    # ))
+    # print("noticias:",noticias)
+    # for n in noticias.noticias:
+    #     print(f"[{n.fecha}] {n.titulo}: {n.contenido}")
 
 if __name__ == "__main__":
     run()
