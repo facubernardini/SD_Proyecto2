12c24bf59c12ac3655f8596abe1bb805d3079535
SolisClaudio <claudio.solis@cs.uns.edu.ar>
2025-07-14 09:12:35 -0300

    creo que anda

--- agente/cliente.py
+++ agente/cliente.py
@@ -20,8 +20,9 @@ def run():
                 "4": lambda: obtener_ultimas_noticias_categoria( stub, id_cliente, password),
                 "5": lambda: ver_categorias_inscripto(stub, id_cliente, password),
                 "6": lambda: borrar_area(stub, id_cliente, password),
+                "7": lambda: agregar_noticia(stub, id_cliente, password),
             }
-            if opcion == "7":
+            if opcion == "8":
                 print("Saliendo...")
                 login = False
             elif opcion in opciones:
@@ -29,11 +30,20 @@ def run():
             else:
                 print("Opción inválida, por favor ingresá un número entre 1 y 6.")
         print("Muchas gracias por utilizar el servicio de noticias CONSORCIO DCIC")
+
+def agregar_noticia(stub, id_cliente, password):
+    titulo = input("Ingrese el titulo de la noticia:")
+    contenido = input("Ingrese el contenido de la noticia: ")
+    area = input("Ingrese el area a la cual pertenece la noticia: ")
+    request = agente_pb2.DatosEnviarNoticia(cliente_id=id_cliente, password=password, titulo=titulo, contenido=contenido,area=area)
+    responde = stub.EnviarNoticia(request)
+    print(f"Respuesta del servidor: {responde.respuesta}")
+
 def borrar_area(stub, id_cliente, password):
     area = input("Ingrese el nombre del area que desea eliminar: ").split()[0]
     request = agente_pb2.DatosBorrarArea(cliente_id=id_cliente ,password=password, area = area)
     response = stub.BorrarArea(request)
-    print(f"Respuesta del servidor: {response.respuesta}")
+    print(f"Respuesta del servidor: ")
 
 
 def ver_categorias_inscripto(stub, id_cliente, password):
@@ -62,11 +72,12 @@ def borrarse_de_una_suscripcion(stub, id_cliente, password):
     print(f"Resultado de la operacion: {response.exito} ")
 
 def suscribirse_nueva_categoria(stub, id_cliente, password):
-    area = input("Ingresá el area a la cual desea suscribirse: ").split()[0]
-    request = agente_pb2.DatosSuscribirNuevaCategoria(cliente_id=id_cliente, area=area, password=password)
-    response = stub.SuscribirNuevaCategoria(request)
-    print("Respuesta del servidor:\n", response.mensaje)
-    print(f"Resultado de la operacion: {response.exito} ")
+    #area = input("Ingresá el area a la cual desea suscribirse: ").split()[0]
+    #request = agente_pb2.DatosSuscribirNuevaCategoria(cliente_id=id_cliente, area=area, password=password)
+    #response = stub.SuscribirNuevaCategoria(request)
+    #print("Respuesta del servidor:\n", response.mensaje)
+    #print(f"Resultado de la operacion: {response.exito} ")
+    print("Servicio en mantenimiento")
 
 def realizar_login(stub):
     from cryptography.fernet import Fernet
@@ -113,7 +124,8 @@ def mostrar_menu():
     print("4. Obtener ultimas noticias")
     print("5. Ver categorias inscripto")
     print("6. Borrar area")
-    print("7. Salir")
+    print("7. Agregar noticia")
+    print("8. Salir")
     print("===============================")
 
 def leer_password_con_asteriscos():
